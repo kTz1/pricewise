e6db1c225c04bc10d1e24f27aa3840aa0de0b6b0
kTz1 <catalin_simon90@yahoo.com>
2024-02-06 13:35:00 +0200

    database setup

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,16 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if(!productUrl) return;
 
   try {
+    connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+    if(!scrapedProduct) return;
+
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
